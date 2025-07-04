5d8481b64c7392ce01d5be2e36a428b62e61662c
whcorkran <hcorkran@seas.upenn.edu>
2025-07-03 22:12:04 -0400

    fixes

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,5 +1,14 @@
 export default {
   name: "Henry Corkran",
-  year: new Date().getFullYear()
-  // resume will be included here
+  year: new Date().getFullYear(),
+
+  bio: {
+    p1: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+          et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.`,
+
+    p2: `Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.`
+  }
 };
