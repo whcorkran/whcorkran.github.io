d1a6bd25b578abefe2a0e1f811c09ec44c2e5afd
whcorkran <hcorkran@seas.upenn.edu>
2025-07-08 16:09:55 -0400

    my fixes

--- src/_data/site.js
+++ src/_data/site.js
@@ -4,7 +4,7 @@ export default {
 
   bio: {
     p1: `Hello!  I am a software engineer working on an MS at UPenn. I was a trader and derivatives analyst on Wall Street for several years before moving to tech after becoming enamored with the ongoing revolution in deep learning and the monumental systems powering it.`,
-    p2: `I have experience building a diverse array of software, including real-time financial analysis systems, databases, web crawlers, and fullstack apps.`,
+    p2: `I have experience building a diverse set of software, including real-time financial analysis systems, databases, web crawlers, and fullstack apps.`,
     p3: `ML is now my primary focus, and I've designed and implemented deep models for fields ranging from biology to board games.`,
   }
 };
